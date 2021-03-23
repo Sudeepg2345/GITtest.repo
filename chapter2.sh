5723ccdaabdc91a7fbccde18d6eb34fb2816c079
sudeepg2345 <sudeepg2345@gmail.com>
2021-03-23 20:20:47 +0530

    commiting chapter1 to develops

---
+++ chapter2.sh
@@ -0,0 +1,17 @@
+# ! /bin/bash        #denote which shell is interpreting the line
+
+#                     denotes comments
+#echo boy             print the output to screen
+
+
+echo "Hello to you\!"  #print output to screen    
+
+set variable_name = "value"
+set name = "sudeep"
+
+set env VARIABLE_NAME value
+set env PRINTER Shakespeare
+
+
+
+
--- script222.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {1..2}
-do
-   mkdir "directory$i"                         #creating directories 
-   MYVAR= touch "file$i"                       #creating files
-   mv $MYVAR >> "GITtest.repo/directory$i"     #move that files to directory
-     
-done   
