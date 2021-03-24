e8a2b4a38ffe82c4d29217d4a902535d754d06f8
sudeepg2345 <sudeepg2345@gmail.com>
2021-03-25 01:35:33 +0530

    commiting 3rd abd 4th chapter

---
+++ chapter4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#ps -ef             #shows all the process
+#ps -ef | grep bin   #display all bin in that process
+#grep 'set' /etc/profile   #search set in /etc/profile
+#ls -l | grep '^d'        #output that begin with a d are printed
+
+
